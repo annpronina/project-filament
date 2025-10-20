369eda388359e05ede0152d2c8745febdb7ceb18
deivisstasuls <deivis.stasuls@gmail.com>
2025-10-20 13:13:23 +0300

    created brand migration and model

--- database/migrations/2025_10_17_123406_create_products_table.php
+++ database/migrations/2025_10_17_123406_create_products_table.php
@@ -6,19 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 // 1️⃣ Products tabula
 return new class extends Migration {
-    public function up()
+    public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('brand')->nullable();
             $table->string('model')->nullable();
-            $table->string('ean')->unique()->nullable();
+            $table->string('ean')->nullable();
             $table->string('category')->nullable();
             $table->json('attributes')->nullable();
+            $table->foreignId('brand_id')
+                ->nullable()
+                ->constrained('brands')
+                ->nullOnDelete();
             $table->timestamps();
         });
     }
+
 
     public function down()
     {
