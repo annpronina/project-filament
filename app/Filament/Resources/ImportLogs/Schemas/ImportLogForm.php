ccdfaee70e88160d84d8041bdac1d46732342f55
KristapsTucs <kristapstucs1@gmail.com>
2025-10-21 18:19:32 +0300

    updated importLogFOrm

--- app/Filament/Resources/ImportLogs/Schemas/ImportLogForm.php
+++ app/Filament/Resources/ImportLogs/Schemas/ImportLogForm.php
@@ -2,15 +2,27 @@
 
 namespace App\Filament\Resources\ImportLogs\Schemas;
 
+use Filament\Forms;
 use Filament\Schemas\Schema;
 
 class ImportLogForm
 {
     public static function configure(Schema $schema): Schema
     {
-        return $schema
-            ->components([
-                //
-            ]);
+        return $schema->schema([
+            Forms\Components\Select::make('shop_id')
+                ->label('Veikals')
+                ->relationship('shop', 'name')
+                ->searchable()
+                ->required(),
+
+            Forms\Components\TextInput::make('file_type')
+                ->label('Faila tips')
+                ->required(),
+
+            Forms\Components\DateTimePicker::make('imported_at')
+                ->label('Importēts')
+                ->required(),
+        ]);
     }
 }
