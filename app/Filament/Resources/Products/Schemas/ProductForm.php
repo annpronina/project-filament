00f3996631b21d39a1897cd223086498923f33bc
KristapsTucs <kristapstucs1@gmail.com>
2025-10-21 18:18:16 +0300

    productForm updated

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -2,15 +2,39 @@
 
 namespace App\Filament\Resources\Products\Schemas;
 
+use Filament\Forms;
 use Filament\Schemas\Schema;
 
 class ProductForm
 {
     public static function configure(Schema $schema): Schema
     {
-        return $schema
-            ->components([
-                //
-            ]);
+        return $schema->schema([
+            Forms\Components\TextInput::make('name')
+                ->label('Nosaukums')
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\TextInput::make('model')
+                ->label('Modelis')
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\Select::make('category')
+                ->label('Kategorija')
+                ->options([
+                    'electronics' => 'Elektronika',
+                    'home' => 'Mājas preces',
+                    'toys' => 'Rotaļlietas',
+                    'clothing' => 'Apģērbi',
+                ])
+                ->searchable(),
+
+            Forms\Components\Select::make('brand_id')
+                ->label('Zīmols')
+                ->relationship('brand', 'name')
+                ->searchable()
+                ->preload(),
+        ]);
     }
 }
