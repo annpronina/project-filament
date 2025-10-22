f9299d7eeab6631c986bd6c94f51ee339f7d3779
Kristers <kvelps@gmail.com>
2025-10-22 17:30:44 +0300

    Final commit for Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    
+    protected $fillable = ['name', 'slug'];
+
+    // Automātiski ģenerē slug no name, jaunam ierakstam
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
 }
